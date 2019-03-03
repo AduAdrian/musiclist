fba01882b3c1f110907f734f1d13d2c7a703c9d2
AduAdrian <47608742+AduAdrian@users.noreply.github.com>
2019-03-03 13:15:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
 import React, { Component } from 'react';
+import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import {TextField, Button} from '@material-ui/core'
-import'./App.css';
+import {TextField, Button} from '@material-ui/core';
+import axios from 'axios';
 
+const API_URL = 'https://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.search&api_key=' + process.env.REACT_APP_LASTFM_APPKEY;
+
+const isEmplt = (str) => str.length === 0;
 class App extends Component {
+state = {
+searchTerm: '',
+artists: []
+}
+onTextChange = (event) => {
+const value = event.target.value;
 
-  state = {
+this.setState({searchTerm: value});
+}
 
-    searchTerm : ""
-  }
+search = (terms) => {
+const request = API_URL + '&artist=' + terms;
 
-  onTextChange = (event) => {
-    const value = event.target.value;
+console.log(request);
 
-    this.setState({searchTerm: value});
-  }
-  onSearchClick= () =>{
-    console.log(this.state.searchTerm);
-  }
+axios.get(request).then((response) => {
+this.setState({ artists: response.data.results.artistmatches.artist});
+})
+}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Spotify Search
-          </Typography>
-          <TextField 
-          placeholder = "Search on Spotify"
-          onChange ={this.onTextChange}
-          value={this.state.searchTerm}
-          />
-          <Button
-           onClick = {this.onSearchClick} 
-          disabled={this.state.searchTerm.length === 0}
-          >
-          Search
-          </Button>
-        </Toolbar>
-      </AppBar>
-        </header>
-      </div>
-    );
-  }
+onSearchClick = () => {
+this.search(this.state.searchTerm);
+}
+//https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=d000cff27039229f64111c30e925a415&format=json
+
+
+render() {
+return (
+<div className="App">
+<header className="App-header">
+<AppBar position="static" color="primary">
+<Toolbar>
+<Typography variant="h6" color="inherit">
+Photos
+</Typography>
+<TextField 
+placeholder="Search on Spotify" 
+onChange={this.onTextChange}
+value={this.state.searchTerm}
+/>
+<Button 
+onClick={this.onSearchClick} 
+disabled={this.state.searchTerm.length === 0}
+>Search
+</Button>
+</Toolbar>
+</AppBar>
+</header>
+<p>{process.env.REACT_APP_LASTFM_APPKEY}</p>
+<ul>
+{this.state.artists.map((artist) => {
+return <li><span>{artist.name}</span></li>
+})}
+</ul>
+
+</div>
+);
+}
 }
 
 export default App;
